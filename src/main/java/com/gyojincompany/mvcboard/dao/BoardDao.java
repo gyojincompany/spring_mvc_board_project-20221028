211f2e5230c80c158e2558603999a68aeee98c94
gyojincompany <luckyred@gmail.com>
2022-10-28 15:34:47 +0900

    BoardDao에 list 메서드 추가

--- src/main/java/com/gyojincompany/mvcboard/dao/BoardDao.java
+++ src/main/java/com/gyojincompany/mvcboard/dao/BoardDao.java
@@ -1,5 +1,10 @@
 package com.gyojincompany.mvcboard.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
 import javax.naming.Context;
@@ -26,8 +31,68 @@ public class BoardDao {
 		}		
 	}
 	
-	public ArrayList<BoardDto> list() {
+	public ArrayList<BoardDto> list() { //게시판 전체 글 목록을 반환하는 메서드
 		
+		ArrayList<BoardDto> dtos = new ArrayList<BoardDto>();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = dataSource.getConnection();
+			String sql = "SELECT * FROM mvc_board ORDER BY bid DESC";
+			//게시글 번호의 내림차순 정렬(최근글이 가장 위에 오도록 함)
+			pstmt = conn.prepareStatement(sql);//sql문 객체 생성
+			rs = pstmt.executeQuery();//SQL을 실행하여 결과값을 반환
+			
+			while(rs.next()) {
+				int bid = rs.getInt("bid");
+				String bname = rs.getString("bname");
+				String btitle = rs.getString("btitle");
+				String bcontent = rs.getString("bcontent");				
+				Timestamp bdate = rs.getTimestamp("bdate");
+				int bhit = rs.getInt("bhit");
+				int bgroup = rs.getInt("bgroup");
+				int bstep = rs.getInt("bstep");
+				int bindent = rs.getInt("bindent");
+				
+//				BoardDto dto = new BoardDto();
+//				dto.setBid(bid);
+//				dto.setBname(bname);
+//				dto.setBtitle(btitle);
+//				dto.setBcontent(bcontent);
+//				dto.setBdate(bdate);
+//				dto.setBhit(bhit);
+//				dto.setBgroup(bgroup);
+//				dto.setBstep(bstep);
+//				dto.setBindent(bindent);
+				
+				BoardDto dto = new BoardDto(bid, bname, btitle, bcontent, bdate, bhit, bgroup, bstep, bindent);
+				dtos.add(dto);				
+			}				
+				
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return dtos;
 		
 	}
 	
