4f4002b8f09e2bdfd75b5fc669fa7f6c9ca8881f
gyojincompany <luckyred@gmail.com>
2022-10-28 15:47:11 +0900

    list 기능추가

--- src/main/java/com/gyojincompany/mvcboard/controller/BoardController.java
+++ src/main/java/com/gyojincompany/mvcboard/controller/BoardController.java
@@ -1,15 +1,22 @@
 package com.gyojincompany.mvcboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gyojincompany.mvcboard.command.BCommand;
+import com.gyojincompany.mvcboard.command.BListCommand;
 
 @Controller
 public class BoardController {
 	
+	BCommand command = null;
+	
 	@RequestMapping(value = "list")
-	public String list() {
+	public String list(Model model) {
 		
-		
+		command = new BListCommand();
+		command.excute(model);
 		
 		return "list";
 	}
